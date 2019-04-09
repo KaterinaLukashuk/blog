78a611808a739fe0e6d153df6de3da310bcbf4fb
KaterinaLukashuk <46955141+KaterinaLukashuk@users.noreply.github.com>
2019-04-09 15:45:20 +0300

    Add files via upload

--- src/main/java/by/javatrenning/blog/Controller/ArticleController.java
+++ src/main/java/by/javatrenning/blog/Controller/ArticleController.java
@@ -1,13 +1,10 @@
 package by.javatrenning.blog.Controller;
 
-import by.javatrenning.blog.data.Article;
-import by.javatrenning.blog.data.Status;
+import by.javatrenning.blog.data.*;
 import by.javatrenning.blog.repos.ArticleRepository;
 
-import by.javatrenning.blog.data.Comment;
-import by.javatrenning.blog.data.User;
-
 import by.javatrenning.blog.repos.CommentRepository;
+import by.javatrenning.blog.repos.TagRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -19,25 +16,29 @@ import java.util.Map;
 
 @Controller
 public class ArticleController {
-        @Autowired
-        private ArticleRepository articleRepo;
-        @Autowired
-        private CommentRepository commentRepo;
+    @Autowired
+    private ArticleRepository articleRepo;
+    @Autowired
+    private CommentRepository commentRepo;
+    @Autowired
+    private TagRepository tagRepo;
 
-        @GetMapping("/page")
-        public String articles(Map<String, Object > model)
-        {
-            model.put("page", "i hate myself and wont to die");
-            Iterable<Article> articles = articleRepo.findAll();
-            model.put("articles", articles);
-            return "page";
-        }
+    @GetMapping("/page")
+    public String articles(Map<String, Object> model) {
+        model.put("page", "i hate myself");
+        Iterable<Article> articles = articleRepo.findAll();
 
-    @GetMapping("/addarticle")
-    public String  artcl(Map<String, Object> model) {
-        return "addarticle";
+
+        model.put("articles", articles);
+        return "page";
     }
 
+
+
+    @GetMapping("/addarticle")
+    public String artcl(Map<String, Object> model) {
+        return "addarticle";
+    }
 
 
     @PostMapping("/addarticle")
@@ -54,14 +55,19 @@ public class ArticleController {
     }
 
     @RequestMapping(value = "/article/{id}")
-    public  String showArticle(
+    public String showArticle(
             @PathVariable(value = "id") int id,
-            Map<String, Object> model)
-    {
-            Article article = articleRepo.findById(id);
-            model.put("article", article);
-       Iterable<Comment> comments = commentRepo.findByArticle(article);
+            Map<String, Object> model) {
+        Article article = articleRepo.findById(id);
+        model.put("article", article);
+
+        Iterable<Comment> comments = commentRepo.findByArticle(article);
         model.put("articlecomments", comments);
+       // Iterable<Tag> tags = tagRepo.findAll();
+        Iterable<Tag> tags = tagRepo.findByArticles(article);
+        //Iterable<Tag> tags = tagRepo.findByTagId(1);
+        model.put("tags", tags);
+
         return "article";
 
     }
@@ -69,21 +75,20 @@ public class ArticleController {
 
     @GetMapping("/articleedit/{article}")
     public String articleEditForm(@PathVariable Article article,
-                                //  @PathVariable User user,
+                                  //  @PathVariable User user,
                                   Map<String, Object> model) {
         model.put("article", article);
-      //  model.put("user", user);
+        //  model.put("user", user);
         return "articleedit";
-        }
+    }
 
     @PostMapping("savearticle")
     public String saveArticle(
-            @RequestParam("articleId")  Article article,
+            @RequestParam("articleId") Article article,
             @RequestParam String title,
             @RequestParam String text,
             @RequestParam Status status,
-            Map<String, Object> model)
-    {
+            Map<String, Object> model) {
         article.setTitle(title);
         article.setText(text);
         article.setStatus(status);
@@ -104,6 +109,16 @@ public class ArticleController {
         model.put("usersartcl", articles);
 
         return "userpage";
+    }
+
+    @RequestMapping("/find")
+    public  String find(
+            @RequestParam("tag") Tag tag,
+            Map<String, Object> model)
+    {
+        //use for search article by tag
+          Iterable<Article> articles = articleRepo.findByTags(tagRepo.findByTagId(1).get(0));
+        return "page";
     }
 }
 
